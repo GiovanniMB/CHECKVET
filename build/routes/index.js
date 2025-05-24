63098a802a14d6a8c62050020a271140139a109c
VaremSuzin <emilianov616@gmail.com>
2025-05-23 19:12:46 -0600

    modified:   build/html/pages/catalogos.ejs 	modified:   build/html/pages/cita.ejs 	modified:   build/html/pages/crear_clinica.ejs 	modified:   build/html/pages/crear_masc.ejs 	modified:   build/html/pages/crear_pers.ejs 	new file:   build/html/pages/login.ejs 	modified:   build/routes/index.js

--- build/routes/index.js
+++ build/routes/index.js
@@ -3,4 +3,29 @@ import { Router} from "express";
 const router = Router();
 router.get('/',(req,res)=>res.render('index'));
 
+router.get('/login',(req,res)=>{
+    res.render('login')
+});
+
+router.get('/crear/persona',(req,res)=>{
+    res.render('crear_pers')
+});
+
+router.get('/catalogos',(req,res)=>{
+    res.render('catalogos')
+
+});
+
+router.get('/cita',(req,res)=>{
+    res.render('cita')
+});
+
+router.get('/crear/mascota',(req,res)=>{
+    res.render('crear_masc')
+});
+
+router.get('/crear/clinica',(req,res)=>{
+    res.render('crear_clinica')
+});
+
 export default router;

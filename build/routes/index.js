0e6c35a224c19aa70d4643ec22e53b2ba8c4773c
VaremSuzin <emilianov616@gmail.com>
2025-06-01 18:23:28 -0600

    deleted:    build/index.js 	modified:   build/routes/index.js 	modified:   estilos.css.map 	modified:   node_modules/.package-lock.json 	modified:   package-lock.json 	modified:   package.json 	deleted:    src/js/app.js
    	.env
    	build/app.js
    	build/middleware/
    	build/test-env.js
    	database/
    	node_modules/aws-ssl-profiles/
    	node_modules/denque/
    	node_modules/dotenv/
    	node_modules/generate-function/
    	node_modules/is-property/
    	node_modules/long/
    	node_modules/lru-cache/
    	node_modules/lru.min/
    	node_modules/mysql2/
    	node_modules/named-placeholders/
    	node_modules/seq-queue/

--- build/routes/index.js
+++ build/routes/index.js
@@ -1,4 +1,5 @@
 import { Router} from "express";
+import { isAuthenticated } from "../middleware/auth.js";
 import { showLogin, login, logout, dashboard,showRegister, register } from '../controller/authController.js';
 
 
@@ -10,8 +11,8 @@ router.post('/register', register);
 
 router.get('/prueba', showLogin);
 router.post('/login', login);
-router.get('/dashboard', dashboard);
-router.get('/logout', logout);
+router.get('/dashboard', isAuthenticated, dashboard);
+router.get('/logout', isAuthenticated, logout);
 
 
 
@@ -20,25 +21,38 @@ router.get('/login',(req,res)=>{
     res.render('login')
 });
 
-router.get('/crear/persona',(req,res)=>{
+
+router.get('/crear/persona', isAuthenticated,(req,res)=>{
     res.render('crear_pers')
 });
 
-router.get('/catalogos',(req,res)=>{
+router.get('/catalogos',isAuthenticated,(req,res)=>{
     res.render('catalogos')
 
 });
 
-router.get('/cita',(req,res)=>{
+router.get('/cita',isAuthenticated,(req,res)=>{
     res.render('cita')
 });
 
-router.get('/crear/mascota',(req,res)=>{
+router.get('/crear/mascota',isAuthenticated,(req,res)=>{
     res.render('crear_masc')
 });
 
-router.get('/crear/clinica',(req,res)=>{
+router.get('/crear/clinica',isAuthenticated,(req,res)=>{
     res.render('crear_clinica')
+});
+
+router.post('/crear/cliente',isAuthenticated,(req,res)=>{
+    const nombre = req.body.nombre;
+    const apellidoP = req.body.apellido;
+    const apellidoM = req.body.apellido2;
+    const rol = req.body.rol;
+    const telefono = req.body.telefono;
+    const correo = req.body.correo;
+    const curp = req.body.curp;
+    const cedula = req.body.cedula;
+    const clinica = req.body.clinica;
 });
 
 export default router;
